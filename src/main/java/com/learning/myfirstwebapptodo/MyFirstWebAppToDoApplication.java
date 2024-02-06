223b98ed511b1657a38ac65ec0838f31bff9d243
Vishal <ssvishal1990@gmail.com>
2024-02-06 14:00:30 +0530

    Initializing Project

---
+++ src/main/java/com/learning/myfirstwebapptodo/MyFirstWebAppToDoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.myfirstwebapptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWebAppToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstWebAppToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/myfirstwebapptodo/MyFirstWebAppToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.myfirstwebapptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebAppToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
